934e425e763d9094fe37f758e21bd568ea72e918
Alcides Schulz <alcides_schulz@hotmail.com>
2024-03-02 16:50:34 -0500

    Update coding standards.

--- SnowfallSimulation/SDL_Snowfall.cpp
+++ SnowfallSimulation/SDL_Snowfall.cpp
@@ -1,61 +1,63 @@
 #include "../SDL_Framework.h"
 
 class Snowflake {
+
 private:
-    Vector2D mPosition;
-    Vector2D mVelocity;
-    Vector2D mAcceleration;
-    int mRadius;
-    float mAngle;
-    int mDirection;
-    int mOffset;
+    Vector2D    position_;
+    Vector2D    velocity_;
+    Vector2D    acceleration_;
+    int         radius_;
+    float       angle_;
+    int         direction_;
+    int         offset_;
+
 public:
     Snowflake(int x, int y)
     {
-        mPosition = Vector2D(x, y);
-        mVelocity = Vector2D(0, 0);
-        mAcceleration = Vector2D(0, 0);
-        mRadius = rand() % 8 + 1;
-        mAngle = (float)(rand() % 6);
-        mDirection = rand() % 2 ? 1 : -1;
-        mOffset = 0;
+        position_ = Vector2D(x, y);
+        velocity_ = Vector2D(0, 0);
+        acceleration_ = Vector2D(0, 0);
+        radius_ = rand() % 8 + 1;
+        angle_ = (float)(rand() % 6);
+        direction_ = rand() % 2 ? 1 : -1;
+        offset_ = 0;
     }
 
     void ApplyForce(Vector2D force)
     {
         Vector2D adjusted_force = Vector2D(force.x, force.y);
-        adjusted_force.Multiply((float)mRadius);
-        mAcceleration.Add(adjusted_force);
+        adjusted_force.Multiply((float)radius_);
+        acceleration_.Add(adjusted_force);
     }
 
     void UpdatePosition()
     {
-        mOffset = (int)(sin(mAngle * 2) * 2 * mRadius);
-        mVelocity.Add(mAcceleration);
-        if (mVelocity.y > (double)(mRadius * 0.2)) {
-            mVelocity.y = (float)(mRadius * 0.2);
+        offset_ = (int)(sin(angle_ * 2) * 2 * radius_);
+        velocity_.Add(acceleration_);
+        if (velocity_.y > (double)(radius_ * 0.2)) {
+            velocity_.y = (float)(radius_ * 0.2);
         }
-        mPosition.Add(mVelocity);
-        mAcceleration.Multiply(0);
-        mAngle += mDirection * (mVelocity.x * mVelocity.x + mVelocity.y * mVelocity.y) / 200;
+        position_.Add(velocity_);
+        acceleration_.Multiply(0);
+        angle_ += direction_ * (velocity_.x * velocity_.x + velocity_.y * velocity_.y) / 200;
     }
 
     Vector2D GetPosition()
     {
-        Vector2D temp = { mPosition.x + mOffset, mPosition.y };
+        Vector2D temp = { position_.x + offset_, position_.y };
         return temp;
     }
 
     int GetRadius() {
-        return mRadius;
+        return radius_;
     }
 };
 
 class SDL_Snowfall : public SDL_Framework
 {
 private:
-    std::vector<Snowflake*> mSnowflakeList;
-    Vector2D mGravity = { 0.0, 0.3 };
+    std::vector<Snowflake*> snowflake_list_;
+    Vector2D                kGravity_ = { 0.0, 0.3 };
 
 public:
     bool UserRender(int elapsed_time) override
@@ -63,25 +65,25 @@ public:
         SDL_SetRenderDrawColor(Renderer(), 0, 0, 0, 0); // black color_
         SDL_RenderClear(Renderer());
         
-        if (mSnowflakeList.size() < 400) {
+        if (snowflake_list_.size() < 400) {
             int x = rand() % WindowWidth();
             int y = (rand() % WindowHeight()) * -1;
             Snowflake *snowflake = new Snowflake(x, y);
-            mSnowflakeList.push_back(snowflake);
+            snowflake_list_.push_back(snowflake);
         }
 
-        for each (auto &snowflake in mSnowflakeList)
+        for each (auto &snowflake in snowflake_list_)
         {
-            snowflake->ApplyForce(mGravity);
+            snowflake->ApplyForce(kGravity_);
             snowflake->UpdatePosition();
             SDL_Point center = { snowflake->GetPosition().IntX(), snowflake->GetPosition().IntY() };
             DrawCircle(center, snowflake->GetRadius(), { 255, 255, 255, 255 }, true);
         }
 
-        for (int i = (int)mSnowflakeList.size() - 1; i >= 0; i--) {
-            Snowflake *snowflake = mSnowflakeList[i];
+        for (int i = (int)snowflake_list_.size() - 1; i >= 0; i--) {
+            Snowflake *snowflake = snowflake_list_[i];
             if (snowflake->GetPosition().y > WindowHeight()) {
-                mSnowflakeList.erase(mSnowflakeList.begin() + i);
+                snowflake_list_.erase(snowflake_list_.begin() + i);
             }
         }
 
--- UniverseProceduralGeneration/SDL_UniverseProceduralGeneration.cpp
+++ UniverseProceduralGeneration/SDL_UniverseProceduralGeneration.cpp
@@ -1,6 +1,6 @@
 #include "../SDL_Framework.h"
 
-constexpr SDL_Color STAR_COLORS[8] =
+constexpr SDL_Color kStarColors[8] =
 {
     {0xFF, 0xFF, 0xFF, 0xFF}, {0xFF, 0xFF, 0xD9, 0xFF}, {0xFF, 0xFF, 0xA3, 0xFF}, {0xC8, 0xC8, 0xFF, 0xFF},
     {0x9D, 0xCB, 0xFF, 0xFF}, {0xFF, 0x9F, 0x9F, 0xFF}, {0xFF, 0x5E, 0x41, 0xFF}, {0x9D, 0x19, 0x28, 0xFF}
@@ -8,35 +8,35 @@ constexpr SDL_Color STAR_COLORS[8] =
 
 struct sPlanet
 {
-    double distance = 0.0;
-    double diameter = 0.0;
-    double foliage = 0.0;
-    double minerals = 0.0;
-    double water = 0.0;
-    double gases = 0.0;
-    double temperature = 0.0;
-    double population = 0.0;
-    bool ring = false;
-    std::vector<double> moons;
+    double  distance = 0.0;
+    double  diameter = 0.0;
+    double  foliage = 0.0;
+    double  minerals = 0.0;
+    double  water = 0.0;
+    double  gases = 0.0;
+    double  temperature = 0.0;
+    double  population = 0.0;
+    bool    ring = false;
+    vector<double> moons;
 };
 
 class StarSystem
 {
 public:
-    std::vector<sPlanet> mPlanets;
-    bool		mStarExists = false;
-    double		mStarDiameter = 0.0f;
-    SDL_Color	mStarColor;
+    vector<sPlanet> planets;
+    bool		start_exists = false;
+    double		start_diameter = 0.0f;
+    SDL_Color	start_color;
 
     StarSystem(uint32_t x, uint32_t y, bool generate_full_system = false)
     {
-        mProcGenSeed = (x & 0xFFFF) << 16 | (y & 0xFFFF);
+        generation_seed_ = (x & 0xFFFF) << 16 | (y & 0xFFFF);
 
-        mStarExists = (GetRandomInt(0, 20) == 1);
-        if (!mStarExists) return;
+        start_exists = (GetRandomInt(0, 20) == 1);
+        if (!start_exists) return;
 
-        mStarDiameter = GetRandomDouble(10.0, 40.0);
-        mStarColor = STAR_COLORS[GetRandomInt(0, 8)];
+        start_diameter = GetRandomDouble(10.0, 40.0);
+        start_color = kStarColors[GetRandomInt(0, 8)];
 
         if (!generate_full_system) return;
 
@@ -74,7 +74,7 @@ public:
                 p.moons.push_back(GetRandomDouble(1.0, 5.0));
             }
 
-            mPlanets.push_back(p);
+            planets.push_back(p);
         }
     }
 
@@ -85,7 +85,7 @@ public:
 
 
 private:
-    uint32_t mProcGenSeed = 0;
+    uint32_t generation_seed_ = 0;
 
     double GetRandomDouble(double min, double max)
     {
@@ -99,9 +99,9 @@ private:
 
     uint32_t GetRandom()
     {
-        mProcGenSeed += 0xe120fc15;
+        generation_seed_ += 0xe120fc15;
         uint64_t tmp;
-        tmp = (uint64_t)mProcGenSeed * 0x4a39b70d;
+        tmp = (uint64_t)generation_seed_ * 0x4a39b70d;
         uint32_t m1 = (uint32_t)((tmp >> 32) ^ tmp);
         tmp = (uint64_t)m1 * 0x12fad5c9;
         uint32_t m2 = (uint32_t)((tmp >> 32) ^ tmp);
@@ -112,29 +112,23 @@ private:
 
 class SDL_UniverseProceduralGeneration : public SDL_Framework
 {
-
 private:
-
-    SDL_Point mGalaxyOffset = { 0,0 };
-
-    bool mStarSelected = false;
-
-    uint32_t mSelectedStarSeed1 = 0;
-    uint32_t mSelectedStarSeed2 = 0;
-
+    SDL_Point   galaxy_offset_ = { 0, 0 };
+    bool        start_is_selected_ = false;
+    uint32_t    selected_start_seed1_ = 0;
+    uint32_t    selected_start_seed2 = 0;
 public:
-
     bool UserRender(int elapsed_time) override
     {
         SDL_SetRenderDrawColor(Renderer(), 0, 0, 0, 0); // black color_
         SDL_RenderClear(Renderer());
 
-        if (PressedKey() == SDLK_w) mGalaxyOffset.y -= 50;
-        if (PressedKey() == SDLK_s) mGalaxyOffset.y += 50;
-        if (PressedKey() == SDLK_a) mGalaxyOffset.x -= 50;
-        if (PressedKey() == SDLK_d) mGalaxyOffset.x += 50;
+        if (PressedKey() == SDLK_w) galaxy_offset_.y -= 50;
+        if (PressedKey() == SDLK_s) galaxy_offset_.y += 50;
+        if (PressedKey() == SDLK_a) galaxy_offset_.x -= 50;
+        if (PressedKey() == SDLK_d) galaxy_offset_.x += 50;
 
-        //std::cout << "(" << mGalaxyOffset.x << "," << mGalaxyOffset.y << ")\n";
+        //std::cout << "(" << galaxy_offset_.x << "," << galaxy_offset_.y << ")\n";
 
         int sectors_x = WindowWidth() / 16;
         int sectors_y = WindowHeight() / 16;
@@ -145,15 +139,15 @@ public:
 
             for (int screen_sector_y = 0; screen_sector_y < sectors_y; screen_sector_y++) {
 
-                uint32_t seed1 = mGalaxyOffset.x + screen_sector_x;
-                uint32_t seed2 = mGalaxyOffset.y + screen_sector_y;
+                uint32_t seed1 = galaxy_offset_.x + screen_sector_x;
+                uint32_t seed2 = galaxy_offset_.y + screen_sector_y;
 
                 StarSystem star(seed1, seed2);
-                if (star.mStarExists)
+                if (star.start_exists)
                 {
                     SDL_Point p = { screen_sector_x * 16 + 8, screen_sector_y * 16 + 8 };
                     
-                    DrawCircle(p, (int)star.mStarDiameter / 8, star.mStarColor, true);
+                    DrawCircle(p, (int)star.start_diameter / 8, star.start_color, true);
 
                     if (vMouse.x == screen_sector_x && vMouse.y == screen_sector_y) {
                         p.x = screen_sector_x * 16 + 8;
@@ -166,23 +160,23 @@ public:
 
         if (IsMouseButtonPressed(LEFT))
         {
-            uint32_t seed1 = mGalaxyOffset.x + vMouse.x;
-            uint32_t seed2 = mGalaxyOffset.y + vMouse.y;
+            uint32_t seed1 = galaxy_offset_.x + vMouse.x;
+            uint32_t seed2 = galaxy_offset_.y + vMouse.y;
 
             StarSystem star(seed1, seed2);
-            if (star.mStarExists)
+            if (star.start_exists)
             {
-                mStarSelected = true;
-                mSelectedStarSeed1 = seed1;
-                mSelectedStarSeed2 = seed2;
+                start_is_selected_ = true;
+                selected_start_seed1_ = seed1;
+                selected_start_seed2 = seed2;
             }
             else
-                mStarSelected = false;
+                start_is_selected_ = false;
         }
 
-        if (mStarSelected)
+        if (start_is_selected_)
         {
-            StarSystem star(mSelectedStarSeed1, mSelectedStarSeed2, true);
+            StarSystem star(selected_start_seed1_, selected_start_seed2, true);
 
             SDL_SetRenderDrawColor(Renderer(), 0, 0, 128, 0); // dark blue color_
             SDL_Rect r = { 8, WindowHeight() - 232, 550, 232 };
@@ -192,11 +186,11 @@ public:
 
             SDL_Point body = { 14, WindowHeight() - 232 / 2 };
 
-            body.x += (int)(star.mStarDiameter * 1.375);
-            DrawCircle(body, (int)(star.mStarDiameter * 1.375), star.mStarColor, true);
-            body.x += (int)((star.mStarDiameter * 1.375) + 8);
+            body.x += (int)(star.start_diameter * 1.375);
+            DrawCircle(body, (int)(star.start_diameter * 1.375), star.start_color, true);
+            body.x += (int)((star.start_diameter * 1.375) + 8);
 
-            for (auto& planet : star.mPlanets)
+            for (auto& planet : star.planets)
             {
                 if (body.x + planet.diameter >= 496) break;
 
